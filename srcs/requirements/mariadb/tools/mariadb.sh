2431da571b3fa59b442b85b975ed0230a007356a
Kewan Silvestre <silvestrekewan@gmail.com>
2026-02-02 10:58:27 -0500

    feat(mariadb): création du script d'initialisation de la base de données
    Le script installe désormais la db, lance le serveur temporaire et
    crée l'utilisateur si la base n'existe pas.

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,6 +1,26 @@
-docker run -it --rm \
-  -e SQL_DATABASE=inception \
-  -e SQL_USER=tonuser \
-  -e SQL_PASSWORD=tonmdp \
-  -e SQL_ROOT_PASSWORD=rootmdp \
-  mariadb
+#!/bin/sh
+
+# COULEURS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
+# Faire des tests avant pour voir si tout est correct
+
+if [ ! -d "/var/lib/mysql/mysql"]; then
+  echo -e "${YELLOW}Downloading MariaDB...${RESET}"
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+  echo -e "${YELLOW}Starting the server...${RESET}"
+  mysqld_safe &
+  sleep 5
+
+  echo -e "${GREEN}MariaDB is on${RESET}"
+
+  mysql -u root -e "FLUSH PRIVILEGES;"
+  mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+  mysql -u root -e "CREATE USER IF NOT EXISTS 'ksilvest'@'%';"
+fi
+
+wait
