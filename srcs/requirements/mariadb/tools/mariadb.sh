f5f76561a745846f2faa6e07d1b7a1f82f314a8b
Kewan Silvestre <silvestrekewan@gmail.com>
2026-02-02 12:00:23 -0500

    fix(mariadb): correction du script d'init et liaison avec le .env
    Correction des problèmes critiques empêchant le démarrage du conteneur :
    - Makefile : ajout du flag --env-file pour charger les variables du .env (fix mot de passe)
    - mariadb.sh : correction de la syntaxe SQL, suppression des espaces parasites
    - mariadb.sh : ajout de l'arrêt propre (shutdown) avant le lancement final pour éviter le crash (PID 1)

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -8,19 +8,27 @@ RESET='\033[0m'
 
 # Faire des tests avant pour voir si tout est correct
 
-if [ ! -d "/var/lib/mysql/mysql"]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
   echo -e "${YELLOW}Downloading MariaDB...${RESET}"
   mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
   echo -e "${YELLOW}Starting the server...${RESET}"
-  mysqld_safe &
+  /usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+
+  echo -e "${YELLOW}Waiting for server startup...${RESET}"
   sleep 5
 
   echo -e "${GREEN}MariaDB is on${RESET}"
 
   mysql -u root -e "FLUSH PRIVILEGES;"
-  mysql -u root -e "CREATE DATABASE ${DB_NAME};"
-  mysql -u root -e "CREATE USER IF NOT EXISTS 'ksilvest'@'%';"
+  mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+  mysql -u root -e "CREATE USER IF NOT EXISTS 'ksilvest'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+  mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO 'ksilvest'@'%';"
+  mysql -u root -e "AlTER usER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'"
+  mysql -u root -e "FLUSH PRIVILEGES;"
+
+  echo -e "${YELLOW}Stopping temporary server...${RESET}"
+  mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
 fi
 
-wait
+exec mysqld_safe --datadir=/var/lib/mysql
