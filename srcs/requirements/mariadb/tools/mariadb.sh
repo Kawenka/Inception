b7bd94f1a561d05cd1c82e2f3bd4a7e569fed3ae
Kawenka <silvestrekewan@gmail.com>
2026-02-02 21:22:23 +0100

    refactor(mariadb): dynamisation de l'utilisateur et correction SQL
    Remplacement des valeurs hardcodées par les variables d'environnement pour plus de flexibilité :
    - .env : ajout de la variable DB_USER
    - mariadb.sh : remplacement de 'ksilvest' par ${DB_USER}
    - mariadb.sh : correction de la casse (typo) sur la commande ALTER USER

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -22,9 +22,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
   mysql -u root -e "FLUSH PRIVILEGES;"
   mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-  mysql -u root -e "CREATE USER IF NOT EXISTS 'ksilvest'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-  mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO 'ksilvest'@'%';"
-  mysql -u root -e "AlTER usER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'"
+  mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+  mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+  mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'"
   mysql -u root -e "FLUSH PRIVILEGES;"
 
   echo -e "${YELLOW}Stopping temporary server...${RESET}"
