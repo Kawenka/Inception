3305c01dbfd67457c766fb52aaf1e3f006a27d0d
kawenka <silvestrekewan@gmail.com>
2026-02-19 18:05:46 +0100

    fix(ftp): disable seccomp_sandbox and clean up startup script

--- srcs/requirements/bonus/ftp/tools/ftp-server.sh
+++ srcs/requirements/bonus/ftp/tools/ftp-server.sh
@@ -8,20 +8,13 @@ RESET='\033[0m'
 if ! id "$FTP_USER" > /dev/null 2>&1; then
     echo -e "${YELLOW}Création de l'utilisateur FTP: $FTP_USER${RESET}"
     
-    # -h /var/www/html : On force son dossier personnel à être celui de WordPress
-    # -s /sbin/nologin : Sécurité, on lui interdit d'ouvrir un vrai terminal
-    # -D : Ne demande pas de mot de passe
     adduser -h /var/www/html -D "$FTP_USER"
     
-    # On lui attribue le mot de passe défini dans le .env
     echo "$FTP_USER:$FTP_PASS" | chpasswd
     
     chown -R "$FTP_USER:$FTP_USER" /var/www/html
 fi
 
-echo "${GREEN}Démarrage du serveur FTP...${RESET}"
+echo -e "${GREEN}Démarrage du serveur FTP...${RESET}"
 
-# La commande 'exec "$@" récupère la ligne CMD du Dockerfile 
-# ("/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf")
-# et remplace le script bash par vsftpd. C'est lui qui gardera le conteneur en vie
 exec "$@"
